cbb9b3cd6018e4614020356e763ad091c88d042a
ERIC BESSON <e-besson@laposte.net>
2024-02-16 17:26:26 +0100

    nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main>
-      <Navbar />
       <div className="App">App</div>
     </main>
   );

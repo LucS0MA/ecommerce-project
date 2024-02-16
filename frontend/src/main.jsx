ee04649291429c3b75ec2c117601c37b6dabe8cb
Florian <f.schaessens@gmail.com>
2024-02-16 11:25:20 +0100

    add Intro with animations

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import App from "./App";
+import "./App.css";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
   },
 ]);
 

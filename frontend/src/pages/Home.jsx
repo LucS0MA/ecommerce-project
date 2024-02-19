8321d29cf450d18f5b476b7eedf0d169fd8568ae
Lucien KOHLI <lucien.kohli@gmail.com>
2024-02-19 10:52:02 +0100

    finish all the visuals for modals

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import BigTitle from "../components/BigTitle";
 import Cookies from "../components/Cookies";
+import Connexion from "../components/Connexion";
 
 function Home() {
   return (
     <>
+      <Connexion />
       <Cookies />
       <BigTitle />
     </>

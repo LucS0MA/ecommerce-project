d6b13af00b87ec8585e1735b00dd8d0d85c23fc8
Ioana Horeanu <ioana.catalina10@yahoo.com>
2024-02-22 11:51:53 +0100

    finish

--- frontend/src/components/Date.jsx
+++ frontend/src/components/Date.jsx
@@ -7,13 +7,23 @@ function Date({ nom, lieu, date }) {
     <body>
       <div className="containerD">
         <div className="dateD">
+          <p className="numberD">12</p>
+          <p className="numberD">-</p>
+          <p className="numberD">13</p>
+          <p className="boldD">JUIN</p>
+          <p className="numberD">2024</p>
           <p className="numberD">{date}</p>
         </div>
         <div className="lineD" />
         <div className="locaD">
+          <p className="placeD">Médiéval de Mecquignies</p>
           <p className="placeD">{nom}</p>
           <div className="adressD">
             <img src={Loca} alt="localisation" className="logoD" />
+            <p className="textD">
+              Mecquignies / Hauts-de-France / France MECQUIGNIES 59570 - Impasse
+              du Culot
+            </p>
             <p className="textD">{lieu}</p>
           </div>
         </div>

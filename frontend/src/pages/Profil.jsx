981cb9cc8378560f5d245c26d7fe068427a5051a
Goodtimenl <gregory.delaloeuf@gmail.com>
2024-03-04 16:26:41 +0000

    add navbar in the profil page

--- frontend/src/pages/Profil.jsx
+++ frontend/src/pages/Profil.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import "../styles/Profil.scss";
 import { ProfileProvider } from "../components/profilcomponents/ProfileContext";
 import ProfilContainer from "../components/profilcomponents/ProfilContainer"; // Importez ProfilContainer
+import NavBar from "../components/Navbar";
+import { ModalProvider } from "../contexts/ConnexionContext";
 
 function Profil() {
   return (
-    <ProfileProvider>
-      <div className="profil-page">
-        <ProfilContainer />{" "}
-        {/* Ceci inclut MenuProfil et le contenu conditionnel */}
-      </div>
-    </ProfileProvider>
+    <ModalProvider>
+      <NavBar />
+      <ProfileProvider>
+        <div className="profil-page">
+          <ProfilContainer />{" "}
+          {/* Ceci inclut MenuProfil et le contenu conditionnel */}
+        </div>
+      </ProfileProvider>
+    </ModalProvider>
   );
 }
 

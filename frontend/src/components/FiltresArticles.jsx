1da03a58e57dd61f0b95c0f6f93623becccc51cb
Eric Besson <150351199+Eric198809@users.noreply.github.com>
2024-03-11 17:13:43 +0100

    filtres ok

--- frontend/src/components/FiltresArticles.jsx
+++ frontend/src/components/FiltresArticles.jsx
@@ -1,13 +1,16 @@
-import "../styles/FiltresArticles.scss";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import line from "../assets/line.svg";
 import Article from "./Article";
+import line from "../assets/line.svg";
+import "../styles/FiltresArticles.scss";
 
 function FiltresArticles() {
-  const [search, setSearch] = useState("");
-  const [rangeValue, setRangeValue] = useState(0);
-  const [thematiqueValue, setThematiqueValue] = useState("");
+  const [search, setSearch] = useState(""); /// / Valeur de la barre de recherche ////
+  const [prixValue, setPrixValue] = useState(200); /// / Valeur de la range prix ////
+  const [thematiqueValue, setThematiqueValue] = useState(null); /// / Valeur inputs thématique ////
+  const [typeValue, setTypeValue] = useState([]);
+  const [CouleurValue, setCouleurValue] = useState(null); /// / Valeur inputs couleur ////
+  const [articles, setArticles] = useState([]); /// / Tableau contenant les articles de la BDD ////
 
   const thematiques = [
     "STEAMPUNK",
@@ -27,24 +30,51 @@ function FiltresArticles() {
   ];
 
   const couleurs = [
-    "#4E5C2C",
-    "#000000",
-    "#82583E",
-    "#E2B95B",
-    "#DF824D",
-    "#720F0F",
-    "#3E7282",
-    "#8D214C",
-    "#5A38A3",
+    { nom: "vert", couleur: "#4E5C2C" },
+    { nom: "noir", couleur: "#000000" },
+    { nom: "marron", couleur: "#82583E" },
+    { nom: "jaune", couleur: "#E2B95B" },
+    { nom: "orange", couleur: "#DF824D" },
+    { nom: "rouge", couleur: "#720F0F" },
+    { nom: "bleu", couleur: "#3E7282" },
+    { nom: "move", couleur: "#8D214C" },
+    { nom: "violet", couleur: "#5A38A3" },
   ];
 
-  const [articles, setArticles] = useState([]);
+  /// / Récuperer les articles de la BDD ////
+  const getArticles = async () => {
+    try {
+      const response = await axios.get("http://localhost:3310/api/articles");
+      setArticles(response.data);
+    } catch (error) {
+      console.error(
+        "Erreur lors du chargement des données depuis l'API",
+        error
+      );
+    }
+  };
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3310/api/articles")
-      .then((res) => setArticles(res.data));
+    getArticles();
   }, []);
+
+  /// / Fonction permettant de filter les articles////
+  const donneesFiltrees = articles.filter((item) => {
+    const filtreParNom = !search || item.nom.includes(search);
+    const filtreParTheme = !thematiqueValue || item.type === thematiqueValue;
+    const filtreParPrix = prixValue >= item.prix;
+    const filtreParType =
+      typeValue.length === 0 || typeValue.includes(item.thematique);
+    const filtreParCouleur = !CouleurValue || item.couleur === CouleurValue;
+
+    return (
+      filtreParTheme &&
+      filtreParNom &&
+      filtreParPrix &&
+      filtreParType &&
+      filtreParCouleur
+    );
+  });
 
   return (
     <main>
@@ -57,7 +87,7 @@ function FiltresArticles() {
           <div className="searchBar_container">
             <input
               className="input_search"
-              type="search"
+              type="texte"
               placeholder="Chercher une création"
               value={search}
               onChange={(e) => setSearch(e.target.value)}
@@ -77,11 +107,12 @@ function FiltresArticles() {
               className="input_range"
               type="range"
               min="0"
-              max="200"
-              defaultValue={rangeValue}
-              onChange={(e) => setRangeValue(e.target.value)}
+              // max="200"
+              defaultValue="200"
+              value={prixValue}
+              onChange={(e) => setPrixValue(e.target.value)}
             />
-            <p>{rangeValue} €</p>
+            <p>{prixValue} €</p>
           </div>
           {/* Thematique */}
           <div className="filtre_container">
@@ -96,7 +127,7 @@ function FiltresArticles() {
                   className="thematique_button"
                   type="button"
                   value={thematique}
-                  onClick={(e) => setThematiqueValue(e.target.value)}
+                  onClick={() => setThematiqueValue(thematique)}
                 >
                   {thematique}
                 </button>
@@ -112,7 +143,13 @@ function FiltresArticles() {
             <div className="types_buttons">
               {types.map((type) => (
                 <div key={type.id}>
-                  <input className="type_button" type="checkbox" id={type} />
+                  <input
+                    className="type_button"
+                    type="checkbox"
+                    id={type}
+                    value={type}
+                    onClick={(e) => setTypeValue(e.target.value)}
+                  />
                   <label htmlFor={type}>{type}</label>
                 </div>
               ))}
@@ -131,7 +168,9 @@ function FiltresArticles() {
                   className="color_button"
                   aria-label="bbb"
                   type="button"
-                  style={{ backgroundColor: couleur }}
+                  value={couleur.nom}
+                  style={{ backgroundColor: couleur.couleur }}
+                  onClick={() => setCouleurValue(couleur.nom)}
                 />
               ))}
             </div>
@@ -139,25 +178,16 @@ function FiltresArticles() {
         </div>
         <div>
           <div className="view_articles">
-            {articles
-              .filter(
-                (article) =>
-                  (article.nom.includes(search) &&
-                    article.prix >= rangeValue &&
-                    article.type === thematiqueValue) ||
-                  thematiqueValue === ""
-              )
-              .map((article) => (
-                <Article
-                  key={article.id}
-                  id={article.id}
-                  image={`http://localhost:3310${article.image}`}
-                  nom={article.nom}
-                  vendeuse={article.vendeuse}
-                  prix={`${article.prix} €`}
-                  isFav={false}
-                />
-              ))}
+            {donneesFiltrees.map((article) => (
+              <Article
+                key={article.id}
+                image={`http://localhost:3310${article.image}`}
+                nom={article.nom}
+                vendeuse={article.vendeuse}
+                prix={`${article.prix} €`}
+                isFav={false}
+              />
+            ))}
             ;
           </div>
         </div>

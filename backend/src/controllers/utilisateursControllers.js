047f6c4e6053990df819aa4a6bfefc82860853c6
Lucien KOHLI <lucien.kohli@gmail.com>
2024-03-12 11:25:07 +0100

    JWT generate on connexion and stocked in localStorage

--- backend/src/controllers/utilisateursControllers.js
+++ backend/src/controllers/utilisateursControllers.js
@@ -1,5 +1,6 @@
 // Import access to database tables
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const models = require("../modelsProviders");
 
 // The B of BREAD - Browse (Read All) operation
@@ -98,8 +99,15 @@ function login(req, res) {
       bcrypt.compare(password, utilisateur.password).then((passwordMatch) => {
         // Si les mots de passe correspondent on crée un token qui va contenir l'id et l'email de l'utilisateur
         if (passwordMatch) {
+          // Création du token avec une expiration d'une heure (vous pouvez ajuster cela selon vos besoins)
+          const token = jwt.sign(
+            { id: utilisateur.id, email: utilisateur.email },
+            process.env.APP_SECRET, // replace with your own secret key
+            { expiresIn: "1h" }
+          );
+
           // là on envoie le token au client avec un message de connexion réussie
-          res.status(200).json({ message: "Connexion réussie" });
+          res.status(200).json({ message: "Connexion réussie", token });
         } else {
           // Si y'a une couille dans le paté bah on renvoie un message d'erreur
           res.status(401).json({ message: "Identifiants incorrects" });

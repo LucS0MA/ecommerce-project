04f20995ed38a2ebedba7960f99667dccbd741f2
Lucien KOHLI <lucien.kohli@gmail.com>
2024-03-13 22:53:56 +0100

    conflicts resolved

--- frontend/src/components/Phares.jsx
+++ frontend/src/components/Phares.jsx
@@ -39,7 +39,7 @@ function Phares() {
               id={article.id}
               image={`http://localhost:3310${article.image}`}
               nom={article.nom}
-              // vendeuse={article.vendeuse}
+              vendeuse={article.vendeuse}
               prix={`${article.prix} €`}
               isFav={false}
             />

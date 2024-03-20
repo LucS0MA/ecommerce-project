debd89aba83080f98a87197e5afcf39db5f78b42
Ioana Horeanu <ioana.catalina10@yahoo.com>
2024-03-20 17:05:04 +0100

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Profil from "./pages/Profil";
 import Catalogue from "./pages/Catalogue";
 import Page404 from "./pages/404";
+import About from "./pages/About";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
   {
     path: "/catalogue",
     element: <Catalogue />,
+  },
+  {
+    path: "/About",
+    element: <About />,
   },
   {
     path: "*",

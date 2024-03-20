2e2d4e5a689951b86897f7a70a72a5239e5da04c
Goodtimenl <gregory.delaloeuf@gmail.com>
2024-03-20 14:27:33 +0000

    modify the profile link if user is a seelie

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,8 @@ import panierIcon from "../assets/panier_icon.svg";
 import utilisateurIcon from "../assets/utilisateur_icon.svg";
 
 function Navbar() {
-  const { toggleModal, authentification, logout } = useConnexionContext();
+  const { toggleModal, authentification, logout, userInfo } =
+    useConnexionContext();
   const [showLinks, setShowlinks] = useState(false);
   const handleShowLinks = () => {
     setShowlinks(!showLinks);
@@ -51,7 +52,7 @@ function Navbar() {
           ) : null}
           <img src={panierIcon} alt="basket_icon" />
           {authentification ? (
-            <Link to="/profil">
+            <Link to={userInfo && userInfo.seelie ? "/admin" : "/profil"}>
               <img src={utilisateurIcon} alt="user_icon" />
             </Link>
           ) : (

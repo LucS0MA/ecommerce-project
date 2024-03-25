979148a06616b8a54d6cbc95cee37ccb4dcaf635
Ioana Horeanu <ioana.catalina10@yahoo.com>
2024-03-25 15:12:00 +0100

    changement pour text vendeuses

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import FooterBis from "../components/FooterBis";
 import Connexion from "../components/Connexion";
@@ -6,6 +7,25 @@ import Sellers from "../components/Sellers";
 import "../styles/About.scss";
 
 function About() {
+  const [selectedSeller, setSelectedSeller] = useState(null);
+
+  const sellerTextMap = {
+    Dahlia: "Texte pour la vendeuse 1",
+    Doireann: "Texte pour la vendeuse 2",
+    Achlys: "Texte pour la vendeuse 3",
+    Elya: "Texte pour la vendeuse 4",
+  };
+
+  const handleSellerSelect = (sellerName) => {
+    setSelectedSeller(sellerName);
+  };
+
+  const getTextForSeller = () => {
+    return selectedSeller
+      ? sellerTextMap[selectedSeller]
+      : "Plongez dans un monde artisanal envoûtant, où chaque création est une porte ouverte vers la magie. Dans ce royaume féérique du Comptoir des Seelies, quatre fées bienveillantes veillent sur chaque création avec une grâce infinie. Chaque pièce est imprégnée de passion et de compétence, faisant du Comptoir un lieu où l'extraordinaire devient réalité.";
+  };
+
   return (
     <ModalProvider>
       <header>
@@ -13,15 +33,8 @@ function About() {
         <Connexion />
       </header>
       <main>
-        <Sellers />
-        <p className="text">
-          Plongez dans un monde artisanal envoûtant, où chaque création est une
-          porte ouverte vers la magie. Dans ce royaume féérique du{" "}
-          <strong>Comptoir des Seelies</strong>, quatre fées bienveillantes
-          veillent sur chaque création avec une grâce infinie. Chaque pièce est
-          imprégnée de passion et de compétence, faisant du Comptoir un lieu où
-          l'extraordinaire devient réalité.
-        </p>
+        <Sellers onSellerSelect={handleSellerSelect} />
+        <p className="text">{getTextForSeller()}</p>
       </main>
       <footer>
         <FooterBis />

c836d0f9de07af9b4d4a146e9d25f3b9814775db
Eric Besson <150351199+Eric198809@users.noreply.github.com>
2024-03-26 09:53:01 +0100

    route ok

--- backend/src/router.js
+++ backend/src/router.js
@@ -23,6 +23,7 @@ const validateUser = require("./middlewares/validateUser");
 
 // PUBLIC
 // routes utilisateurs
+
 router.post("/utilisateurs", validateUser, utilisateursControllers.add);
 router.post("/auth/login", utilisateursControllers.login);
 
@@ -70,6 +71,7 @@ router.use(isAdmin);
 
 // route utilisateurs
 router.get("/utilisateurs", utilisateursControllers.browse);
+router.get("/clients", utilisateursControllers.browseClientsOrders);
 
 // routes articles
 router.post("/articles", articlesControllers.add);
---
+++ frontend/src/components/admincomponents/ClientsAdmin.jsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "../../styles/ClientsAdmin.scss";
+
+function ClientsAdmin() {
+  const [clientsList, setClientsList] = useState([]);
+  const [isFiltered, setIsFiltered] = useState(false);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    axios
+      .get(`http://localhost:3310/api/clients`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // Inclusion du jeton JWT
+        },
+      })
+      .then((response) => setClientsList(response.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const sortByOrders = () => {
+    if (isFiltered === false) {
+      const sortCommands = clientsList.sort(
+        (a, b) => b.nombre_de_commandes - a.nombre_de_commandes
+      );
+      setClientsList(sortCommands);
+      setIsFiltered(true);
+    }
+
+    if (isFiltered === true) {
+      setClientsList(clientsList.sort((a, b) => a.id - b.id));
+      setIsFiltered(false);
+    }
+  };
+
+  return (
+    <div className="clients-list-main-container">
+      <h1>LISTE DES CLIENTS</h1>
+      <section className="clients-list-container">
+        <div className="filters-clients-list">
+          <button type="button" onClick={sortByOrders}>
+            {isFiltered ? "ANNULER LE FILTRE" : "TRIER PAR CLIENT FIDELE"}
+          </button>
+          <input
+            className="input_search"
+            type="search"
+            placeholder="rechercher un article"
+          />
+        </div>
+        <div className="clients-list-array-labels">
+          <ul>
+            <li>NOM DU CLIENT</li>
+            <li>PRENOM</li>
+            <li>NOMBRE DE COMMANDES</li>
+            <li>DATE D'INSCRIPTION</li>
+          </ul>
+          <div className="clients-list-details">
+            {clientsList.map((client) => (
+              <ul key={client.id}>
+                <li>{client.nom}</li>
+                <li>{client.prénom}</li>
+                <li>{client.nombre_de_commandes}</li>
+                <li>01/01/2021</li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default ClientsAdmin;

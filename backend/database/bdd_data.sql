b6394ee98a559080454497195a8175c727f3ecfd
Mattéo Donatelli <matteo.donatelli1@gmail.com>
2024-03-27 11:15:09 +0100

    conflict fixed

--- backend/database/bdd_data.sql
+++ backend/database/bdd_data.sql
@@ -340,25 +340,3 @@ INSERT INTO commande_article (quantité, commandes_id, articles_id)
 VALUES
 (1, 3, 4), -- 1x ILLUSTRATION SIRENE
 (2, 3, 2); -- 2x PELUCHE CHAMPIGNON
-<<<<<<< HEAD
-
-
-INSERT INTO utilisateurs (
-  nom,  prénom, date_de_naissance, telephone, email, password, adresse1, CP, ville, pays, seelie
-) VALUES (
-  "admin", "seelie", "2000-06-19", 0607593244, "admin@gmail.com", "$2b$10$qjVeymvkdHT9NWcxGsLSGujegcHnWWv/GFEcWhQMYiS9JqT6hohYa", "6 rue de la Moutarde", 67000, "Strasbourg", "France", 1 
-);
-
-
-
-INSERT INTO paiements (
-  titulaire, numero, expiration, cvv
-) VALUES (
-  'Fée Clochette', '1111222233334444', '2026-07-01 00:00:00', '235'
-),(
-  'Peter pan', '5555666677778888', '2027-03-01 00:00:00', '457'
-), (
-  'Capitaine Crochet', '9999111122223333', '2028-05-01 00:00:00', '679'
-);
-=======
->>>>>>> dev

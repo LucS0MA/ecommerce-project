70b689ea09adf44f54382c4988c2783f89dbfac8
Goodtimenl <gregory.delaloeuf@gmail.com>
2024-03-28 14:32:53 +0100

    back validate basket

--- backend/src/models/PanierManager.js
+++ backend/src/models/PanierManager.js
@@ -50,6 +50,20 @@ class PanierManager extends AbstractManager {
 
     return rows;
   }
+
+  async validatePanier(utilisateurId) {
+    const articlesDansLePanier = await this.readAll(utilisateurId);
+    const updatePromises = articlesDansLePanier.map((article) =>
+      this.database.query(
+        `UPDATE articles SET nb_ventes = nb_ventes + ? WHERE id = ?`,
+        [article.quantité, article.articles_id]
+      )
+    );
+    await Promise.all(updatePromises);
+
+    // Vider le panier après la mise à jour
+    await this.delete(utilisateurId);
+  }
 }
 
 module.exports = PanierManager;
